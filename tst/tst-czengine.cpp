bf383d2771fd7a7c3d1a7901730135fefd09e5e0
Kenneth Cornett <KCornett.AeroSW@gmail.com>
2024-08-03 17:01:41 -0500

    Add filesystem usage to ensure output dir exists for open.

--- tst/tst-czengine.cpp
+++ tst/tst-czengine.cpp
@@ -2,20 +2,27 @@
 #include "gmock/gmock.h"
 
 #include <filesystem>
-#include <fstream>
 #include <fcntl.h>
 
 namespace fs = std::filesystem;
 
-// struct OutputBuffers {
-//     std::streambuf * original_cout_buffer;
-//     std::ofstream output_file;
-// } output_config;
-
+/**
+ * Configures C's `printf` method to print to `./output/tst-czengine.result.txt`.
+ * 
+ * Method utilizes C++'s <filesystem> library to ensure the directory "output" exists.
+ * It then utilizes C's <fcntl.h> library to open the `tst-czengine.result.txt` file in
+ * the output directory and configures the stdout to print into this file.
+ * Source: https://stackoverflow.com/a/35186153/7044902
+ */
 void setup() {
     int fd;
-    const char * filename = "./output/tst-czengine.result.txt";
-    fd = open(filename, O_WRONLY | O_CREAT, 0644);
+    fs::path dir_path{"./output"};
+    fs::path filename = dir_path / "tst-czengine.result.txt";
+    if (!fs::is_directory(dir_path) && !fs::create_directory(dir_path)) {
+        perror("Failed to find and create `./output`.");
+        std::exit(1);
+    }
+    fd = open(filename.string().c_str(), O_WRONLY | O_CREAT, 0644);
     if (fd == -1) {
         perror("Open failed");
         std::exit(1);
@@ -26,18 +33,10 @@ void setup() {
     }
 }
 
-// void teardown() {
-//     if (output_config.output_file.is_open()) {
-//         output_config.output_file.close();
-//     }
-//     std::cout.rdbuf(output_config.original_cout_buffer);
-// }
-
 int main(int argc, char **argv) {
     setup();
     testing::InitGoogleTest(&argc, argv);
     testing::InitGoogleMock(&argc, argv);
     int result = RUN_ALL_TESTS();
-    // teardown();
     return result;
 }
